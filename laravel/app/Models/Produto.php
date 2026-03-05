800f380bc3af2642cb0db0634c9aae331bddbc01
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2026-03-05 20:01:50 -0300

    samylle: adicionando soft delete ao produto

--- laravel/app/Models/Produto.php
+++ laravel/app/Models/Produto.php
@@ -5,8 +5,14 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Produto extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['nome', 'descricao', 'preco', 'categoria'];
+
+    protected $hidden = [
+        'deleted_at',
+    ];
 }
